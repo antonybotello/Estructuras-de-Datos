97466b79ec1ee87738ec021bdeda892c3e994ebf
Antony Botello <antonybotello@gmail.com>
2024-07-03 09:16:22 -0500

    first commit

---
+++ 03-JUL/arreglos/src/App.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+        Scanner teclado = new Scanner(System.in);
+
+        String[] estudiantes1 = new String[7];
+        String[] estudiantes2 = { null, "Mariana", "Samuel", "Nelson", "Veronica", "Vanesa", "" };
+
+        for (int i = 0; i < estudiantes1.length; i++) {
+            System.out.println(estudiantes1[i]);
+        }
+        System.out.println("#############");
+        for (int i = 0; i < estudiantes2.length; i++) {
+            System.out.println(estudiantes2[i]);
+        }
+        int opcion = 9;
+        int pos;
+        while (opcion != 0) {
+            System.out.println("Digite la opción del menú para modificar el arreglo estudiantes1");
+            System.out.println("1. Agregar por posición");
+            System.out.println("2. Buscar por posición");
+            opcion= teclado.nextInt();
+            switch (opcion) {
+                case 1:
+                    System.out.println("Digite la posición del estudiante");
+                    pos = teclado.nextInt();
+                    System.out.println("Digite el nombre del estudiante");
+                    teclado.nextLine();
+                    String val = teclado.nextLine();
+                    agregarPorPosicion(pos, estudiantes1, val);
+                    break;
+                case 2:
+                    System.out.println("Digite la posición del estudiante");
+                    pos = teclado.nextInt();
+                    String estudiante= buscarPorPosicion(pos, estudiantes1);
+                    System.out.println(estudiante);
+                    break;
+                case 0:
+                    System.out.println("Hasta pronto!");
+                    break;
+                default:
+                    System.out.println("Digite una opción válida");
+                    break;
+            }
+        }
+    }
+
+    public static void agregarPorPosicion(int pos, String[] vector, String valor) {
+        vector[pos] = valor;
+    }
+
+    public static String buscarPorPosicion(int pos, String[] vector) {
+        return vector[pos];
+    }
+}
