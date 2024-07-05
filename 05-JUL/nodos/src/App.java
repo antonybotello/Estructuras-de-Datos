2e9df3164f64f4aaddc5b1885a8c41d57a7c779c
Antony Botello <antonybotello@gmail.com>
2024-07-05 09:10:59 -0500

    Menú nodo

--- 05-JUL/nodos/src/App.java
+++ 05-JUL/nodos/src/App.java
@@ -1,11 +1,60 @@
+import java.util.Scanner;
+
 public class App {
     private Nodo primero;
     private Nodo ultimo;
+    static Scanner teclado = new Scanner(System.in);
 
     public static void main(String[] args) throws Exception {
-       
-        
 
+        int opcion = 9;
+
+        while (opcion != 0) {
+
+            System.out.println("__________MENÚ__________");
+            System.out.println("Seleccione una opción del menú");
+            System.out.println("1. Crear Lista");
+            System.out.println("2. Imprimir Lista");
+            System.out.println("3. Agregar nodo al inicio");
+            System.out.println("4. Agregar nodo al final");
+            System.out.println("5. Buscar un elemento en la lista");
+            System.out.println("6. Borrar nodo al inicio");
+            System.out.println("7. Borrar nodo al final");
+            System.out.println("0. SALIR");
+
+            opcion= teclado.nextInt();
+
+            switch (opcion) {
+                case 1: //Escriba su nombre
+                    
+                    break;
+                case 2:
+                    
+                    break;
+                case 3:
+                    
+                    break;
+                case 4:
+                    
+                    break;
+                case 5:
+                    
+                    break;
+                case 6:
+                    
+                    break;
+                case 7:
+                    
+                    break;
+                case 0:
+                    
+                    break;
+            
+                default:
+                    break;
+            }
+
+        }
     }
 
     public void anexarInicioLista(int info) {
