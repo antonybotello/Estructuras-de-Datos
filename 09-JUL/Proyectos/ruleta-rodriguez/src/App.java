8c3bfc278811c149f5e78395116ef49f42e94311
Laura2952 <laura.rodriguezr@usantoto.edu.co>
2024-07-09 11:07:58 -0500

    Create App.java

---
+++ 09-JUL/Proyectos/ruleta-rodriguez/src/App.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class App { 
+    private static Nodo primero; 
+    private static Nodo ultimo; 
+    static Scanner teclado = new Scanner(System.in);
+    static Lista lst;
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        
+    }
+}
