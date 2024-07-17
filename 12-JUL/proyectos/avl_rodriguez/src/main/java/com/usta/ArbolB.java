9ea28cc9d31baa197abfe26f741414863f5f9472
Laura2952 <laura.rodriguezr@usantoto.edu.co>
2024-07-17 11:32:10 -0500

    nuevo

--- 12-JUL/proyectos/avl_rodriguez/src/main/java/com/usta/ArbolB.java
+++ 12-JUL/proyectos/avl_rodriguez/src/main/java/com/usta/ArbolB.java
@@ -1,5 +1,6 @@
 package com.usta;
 
+<<<<<<< HEAD
 import java.io.FileWriter;
 import java.io.PrintWriter;
 
@@ -35,10 +36,75 @@ public class ArbolB {
                 balancear(actual);
             } else {
                 insertar(actual.derecha, nuevo);
+=======
+public class ArbolB {
+    Nodo nuevo;
+    Nodo raiz;
+    Nodo temp;
+    Nodo temp2;
+    int altura;
+    int altura2;
+
+    public ArbolB() {
+        nuevo = null;
+        raiz = null;
+        this.altura = 0;
+        this.altura2 = 0;
+    }
+
+    /*
+     * * Método de alta de nodos, se encarga de dar de alta los nuevos datos
+     * queingrese el usuario *
+     */
+    public void alta(int dato) {
+        if (raiz == null) // se verica si no existe aún un árbol
+        {
+            raiz = new Nodo();
+            raiz.dato = dato;
+            raiz.derecha = null;
+            raiz.izquierda = null;
+            raiz.padre = null;
+            raiz.balance = 0;
+        } else// Caso contrario si ya existe
+        {
+            temp = raiz;
+            while (temp != null) { // si el dato es mayor y hay espacio
+                if ((dato > temp.dato) && (temp.derecha == null)) {
+                    nuevo = new Nodo();
+                    nuevo.dato = dato;
+                    nuevo.izquierda = null;
+                    nuevo.derecha = null;
+                    temp.derecha = nuevo;
+                    nuevo.padre = temp;
+                    nuevo.balance = 0;
+                    balanceAlta(nuevo.padre, 1);
+                    break;
+                } // si el dato es menor y hay espacio
+                else if ((dato < temp.dato) && (temp.izquierda == null)) {
+                    nuevo = new Nodo();
+                    nuevo.dato = dato;
+                    nuevo.izquierda = null;
+                    nuevo.derecha = null;
+                    temp.izquierda = nuevo;
+                    nuevo.padre = temp;
+                    nuevo.balance = 0;
+                    balanceAlta(nuevo.padre, -1);
+                    break;
+                } // si el dato es igual a otro ya agregado
+                else if (dato == temp.dato) {
+                    // Ya existe ese dato en el árbol
+                    break;
+                } // si hay más datos a la derecha
+                else if (dato > temp.dato)
+                    temp = temp.derecha; // si hay más datos a la izquierda
+                else
+                    temp = temp.izquierda;
+>>>>>>> 9fcdc4c (nuevo)
             }
         }
     }
 
+<<<<<<< HEAD
     private void balancear(Nodo nodo) {
         while (nodo != null) {
             setBalance(nodo);
@@ -214,3 +280,231 @@ public class ArbolB {
         return raiz.getCodigoInterno();
     }
 }
+=======
+    public void balanceAlta(Nodo nodo, int incremento) {
+        int tipo = -1;
+        while (nodo != null) {
+            nodo.balance = nodo.balance + incremento; // Reajusta balances
+            if (nodo.balance == 0) // Si es igual a 0 se detiene
+                break;
+            else {
+                tipo = tipoRotacion(nodo); // llamamos al método tipoRotacion
+                if (tipo == 0) // No hay necesidad de rotar
+                {
+                    if (nodo.padre != null) {
+                        if (nodo.dato < nodo.padre.dato) {
+                            incremento = -1;
+                        } else {
+                            incremento = 1;
+                        }
+                    }
+                    nodo = nodo.padre;
+                }
+                if (tipo == 1) // Rotación sencilla izquierda
+                {
+                    rotacionSI(nodo, nodo.derecha);
+                    break;
+                }
+                if (tipo == 2) // Rotación doble derecha-izquierda
+                {
+                    rotacionSD(nodo.derecha, nodo.derecha.izquierda);
+                    rotacionSI(nodo, nodo.derecha);
+                    break;
+                }
+                if (tipo == 3) // Rotación sencilla derecha
+                {
+                    rotacionSD(nodo, nodo.izquierda);
+                    break;
+                }
+                if (tipo == 4)// Rotación doble izquierda-derech
+                {
+                    rotacionSD(nodo, nodo.izquierda);
+                    break;
+                }
+            }
+        }
+    }
+
+    public int tipoRotacion(Nodo pr) {
+        if (pr.balance == 2) // Positivo
+        {
+            if ((pr.derecha.balance == 0) || (pr.derecha.balance == 1))
+                return 1;
+            else if (pr.derecha.balance == -1)
+                return 2;
+        } else if (pr.balance == -2) // Negativo
+        {
+            if ((pr.izquierda.balance == 0) || (pr.izquierda.balance == -1))
+                return 3;
+            else if (pr.izquierda.balance == 1)
+                return 4;
+        }
+        return 0;
+    }
+
+    /**
+     * El método rotacionSI se encarga de realizar la rotación sencilla a
+     * laizquierda
+     * recibe como datos el nodo del problema y su hijo derecho
+     * 
+     * @param pr
+     * @param hijo_der
+     */
+    public void rotacionSI(Nodo pr, Nodo hijo_der) {
+        int w, balt;
+        temp = hijo_der.izquierda;
+        if (pr == raiz) {
+            raiz = hijo_der;
+            hijo_der.padre = null;
+        } else {
+            if (pr.dato > pr.padre.dato) {
+                pr.padre.derecha = hijo_der;
+            } else {
+                pr.padre.izquierda = hijo_der;
+            }
+            hijo_der.padre = pr.padre;
+        }
+        hijo_der.izquierda = pr;
+        pr.derecha = temp;
+        pr.padre = hijo_der;
+        if (temp != null) {
+            temp.padre = pr;
+        }
+        w = pr.balance;
+        pr.balance = (w - 1) - Math.max(hijo_der.balance, 0);
+        balt = Math.min((w - 2), (w + hijo_der.balance - 2));
+        hijo_der.balance = Math.min(balt, (hijo_der.balance - 1));
+    }
+
+    /**
+     * El método rotacionSD se encarga de realizar la rotación sencilla a laderecha
+     * recibe como datos el nodo del problema y su hijo izquierdo * @param pr
+     * * @param hijo_izq
+     */
+    public void rotacionSD(Nodo pr, Nodo hijo_izq) {
+        int w, balt;
+        temp = hijo_izq.derecha;
+        if (pr == raiz) {
+            raiz = hijo_izq;
+            hijo_izq.padre = null;
+        } else {
+            if (pr.dato > pr.padre.dato) {
+                pr.padre.derecha = hijo_izq;
+            } else {
+                pr.padre.izquierda = hijo_izq;
+            }
+        }
+        hijo_izq.padre = pr.padre;
+        hijo_izq.derecha = pr;
+        pr.izquierda = temp;
+        pr.padre = hijo_izq;
+        if (temp != null) {
+            temp.padre = pr;
+        }
+        w = pr.balance;
+        pr.balance = (w + 1) - Math.min(hijo_izq.balance, 0);
+        balt = Math.min((w + 2), (w - hijo_izq.balance + 2));
+        hijo_izq.balance = Math.max(balt, (hijo_izq.balance + 1));
+    }
+
+    public void eliminarelemento(int elemento) {
+        if (raiz != null) {
+            temp = raiz;
+            if (temp.dato == elemento) {
+                raiz = reemplazar(raiz);
+            } else {
+                Nodo actual, par = raiz;
+                boolean foun = false;
+                if (elemento < temp.dato)
+                    actual = raiz.izquierda;
+                else
+                    actual = raiz.derecha;
+                while (actual != null && !foun) {
+                    if (elemento == actual.dato) {
+                        foun = true;
+                        if (actual == par.izquierda) {
+                            par.izquierda = reemplazar(actual);
+                        } else {
+                            par.derecha = reemplazar(actual);
+                        }
+                    } else {
+                        par = actual;
+                        if (elemento < actual.dato)
+                            actual = actual.izquierda;
+                        else
+                            actual = actual.derecha;
+                    }
+                }
+                if (!foun)
+                    System.out.println("Elemento no encontrado");
+            }
+        }
+    }
+
+    public Nodo reemplazar(Nodo nu) {
+        Nodo result = null;
+        if ((nu.izquierda == null) && (nu.derecha == null))
+            result = null;
+        else if ((nu.izquierda != null) && (nu.derecha == null))
+            result = nu.izquierda;
+        else if ((nu.izquierda == null) && (nu.derecha != null))
+            result = nu.derecha;
+        else {
+            Nodo actual = nu.derecha;
+            Nodo pa = nu;
+            while (actual.izquierda != null) {
+                pa = actual;
+                actual = actual.izquierda;
+            }
+            if (nu.derecha == actual)
+                actual.izquierda = nu.izquierda;
+            else {
+                pa.izquierda = actual.derecha;
+                actual.derecha = nu.derecha;
+                actual.izquierda = nu.izquierda;
+            }
+            result = actual;
+        }
+        return result;
+    }
+
+    public void eliminaArbol() {
+        if (raiz != null)
+            eliminArbol(raiz);
+    }
+
+    private void eliminArbol(Nodo raiz) {
+        if (raiz != null) {
+            eliminArbol(raiz.izquierda);
+            eliminArbol(raiz.derecha);
+            raiz = null;
+        }
+    }
+
+    public boolean buscar(int buscado) {
+        if (raiz != null)
+            return (raiz.buscarNodo(buscado) != null);
+        else
+            return false;
+    }
+
+    public void visualizar(int opc) {
+        if (raiz != null) {
+            if (opc == 1) {
+                System.out.println("Elementos del arbol: recorridoEnorden \n");
+                raiz.imprimirEnOrden();
+            } else if (opc == 2) {
+                System.out.println("\nElementos del arbol: recorrido Preorden\n");
+            } else if (opc == 3) {
+                raiz.imprimirPreorden();
+                System.out.println("\nElementos del arbol: recorrido Postorden\n");
+                raiz.imprimirPostorden();
+            }
+        }
+    }
+    
+    public void graficar(String path) {
+        raiz.graficar(path);
+    }
+}
+>>>>>>> 9fcdc4c (nuevo)
