d05b5d4e4ff443728a900710b031303ea9f46b91
VeronicaPaez <veronicapaez29@gmail.com>
2024-07-18 09:19:18 -0500

    grafos

---
+++ 18-JUL/proyectos/grafos-paez/Nodo.java
@@ -0,0 +1,29 @@
+public class Nodo {
+
+    private int vertice;
+    private Nodo siguiente;
+
+    public Nodo() {
+        this.vertice = 0;
+        this.siguiente = null;
+    }
+
+    public int getVertice() {
+        return vertice;
+    }
+
+    public void setVertice(int vertice) {
+        this.vertice = vertice;
+    }
+
+    public Nodo getSiguiente() {
+        return siguiente;
+}
+    public void setSiguiente(Nodo siguiente) {
+        this.siguiente = siguiente;
+    }
+}
+
+    
+
+    
